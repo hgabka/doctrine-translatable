c12be14bbce479a1fc1db96fd00ad37ab4806783
hgabka <hgabka@gmail.com>
2020-10-23 22:10:04 +0200

    Update TranslationInterface.php

--- lib/Hgabka/Doctrine/Translatable/TranslationInterface.php
+++ lib/Hgabka/Doctrine/Translatable/TranslationInterface.php
@@ -1,13 +1,6 @@
 <?php
 
-/*
- * (c) Prezent Internet B.V. <info@prezent.nl>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
-namespace Prezent\Doctrine\Translatable;
+namespace Hgabka\Doctrine\Translatable;
 
 /**
  * Interface for translation entities
