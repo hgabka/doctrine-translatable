e2c77aed14d40b34088ed3a13d7f4a5ebdc69a16
hgabka <hgabka@gmail.com>
2020-10-23 22:26:57 +0200

    Update TranslatableMetadata.php

--- lib/Hgabka/Doctrine/Translatable/Mapping/TranslatableMetadata.php
+++ lib/Hgabka/Doctrine/Translatable/Mapping/TranslatableMetadata.php
@@ -1,13 +1,6 @@
 <?php
 
-/*
- * (c) Prezent Internet B.V. <info@prezent.nl>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
-namespace Prezent\Doctrine\Translatable\Mapping;
+namespace Hgabka\Doctrine\Translatable\Mapping;
 
 use Metadata\MergeableClassMetadata;
 use Metadata\MergeableInterface;
