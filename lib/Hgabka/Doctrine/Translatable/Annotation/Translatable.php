bfcbca6d93b0725ef353c74f4c5ce6efb9d04ed8
hgabka <hgabka@gmail.com>
2022-05-11 15:06:41 +0200

    Update Translatable.php

--- lib/Hgabka/Doctrine/Translatable/Annotation/Translatable.php
+++ lib/Hgabka/Doctrine/Translatable/Annotation/Translatable.php
@@ -10,10 +10,11 @@ use Doctrine\Common\Annotations\NamedArgumentConstructorAnnotation;
  * This annotation indicates the many-to-one relation to the translatable.
  *
  * @Annotation
+ * @NamedArgumentConstructor
  * @Target("PROPERTY")
  */
 #[\Attribute(\Attribute::TARGET_PROPERTY)]
-class Translatable implements NamedArgumentConstructorAnnotation
+class Translatable
 {
     public function __construct(public ?string $targetEntity = null, public ?string $referencedColumnName = 'id')
     {
