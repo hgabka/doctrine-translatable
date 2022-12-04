ea2e6b8c66dffb3a8b48c4ab227ece69dc0d1a62
hgabka <hgabka@gmail.com>
2022-12-04 11:28:07 +0100

    Update TranslatableListener.php

--- lib/Hgabka/Doctrine/Translatable/EventListener/TranslatableListener.php
+++ lib/Hgabka/Doctrine/Translatable/EventListener/TranslatableListener.php
@@ -41,7 +41,7 @@ class TranslatableListener implements EventSubscriber
      *
      * @param MetadataFactory $factory
      */
-    public function __construct(private MetadataFactory $metadataFactory) {}
+    public function __construct(private readonly MetadataFactory $metadataFactory) {}
 
     /**
      * Get the current locale
